32a297fb36c8a9bc8c831b565dcbc650bd371751
Benedek Fazekas <benedek.fazekas@gmail.com>
2020-02-12 18:12:20 +0000

    rename img-dir->imig-dir-prefix

--- src/thomasa/morpheus/main.clj
+++ src/thomasa/morpheus/main.clj
@@ -1,11 +1,11 @@
 (ns thomasa.morpheus.main
   (:require [thomasa.morpheus.core :as m]))
 
-(defn -main [img-dir & paths]
+(defn -main [img-dir-prefix & paths]
   (let [analysis (m/lint-analysis paths)
         graph (m/var-deps-graph analysis)
         nodes (map
                (fn [{:keys [ns name]}] (str ns "/" name))
                (:var-definitions analysis))]
     (doseq [node nodes]
-      (m/node-subgraph->file img-dir graph node))))
+      (m/node-subgraph->file img-dir-prefix graph node))))

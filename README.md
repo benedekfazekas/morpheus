2e530b35e013fa0ffce727a9cd54ac810292b953
Benedek Fazekas <benedek.fazekas@gmail.com>
2020-02-15 15:51:26 +0000

    add help string, example svg, README changes

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-clj-kondo based tool to generate dependency graph(s) for variables.
+# Morpheus
+
+Generate dependency graph(s) for variables.
+
+## Usage
+
+Add an alias for `morpheus` in your deps.edn.
+
+Run it in your project, provide directory to generate output files into -- directory needs to exist, format -- png, svg and dot is supported, latter is default -- and a list of paths to analyse.
+
+Morpheus will generate a file per project variable with its dependency graph where nodes are other variables in the project or in one of the dependencies of the project.
+
+```
+clj -A:morpheus -m thomasa.morpheus.main -d graphs2 -f png src test
+```
+
+Uses [clj-kondo](https://github.com/borkdude/clj-kondo)
--- src/thomasa/morpheus/main.clj
+++ src/thomasa/morpheus/main.clj
@@ -4,8 +4,14 @@
             [clojure.java.io :as io]
             [clojure.string :as str]))
 
+(defn usage [options-summary]
+  (str/join
+   "\n"
+   ["Usage: morpheus -d DIR -f FORMAT paths"
+    options-summary]))
+
 (defn -main [& args]
-  (let [{:keys [arguments errors] {:keys [dir format]} :options}
+  (let [{:keys [arguments errors summary] {:keys [dir format help]} :options}
         (cli/parse-opts
          args
          [["-d" "--dir DIR" "Directory to save output files to"
@@ -13,8 +19,18 @@
            :validate [#(.exists %)]]
           ["-f" "--format FORMAT" "dot, png, svg"
            :default "dot"
-           :validate [#{"dot" "png" "svg"}]]])]
-    (if-not errors
+           :validate [#{"dot" "png" "svg"}]]
+          ["-h" "--help"]])]
+    (cond
+      errors
+      (println
+       "Errors occured while parsing command:\n"
+       (str/join "\n" errors))
+
+      help
+      (println (usage summary))
+
+      :default
       (let [analysis (m/lint-analysis arguments)
             graph    (m/var-deps-graph analysis)
             nodes    (map
@@ -22,6 +38,4 @@
                       (:var-definitions analysis))]
         (doseq [node nodes]
           (m/node-subgraph->file dir format graph node)))
-      (println
-       "Errors occured while parsing command:\n"
-       (str/join "\n" errors)))))
+      )))
